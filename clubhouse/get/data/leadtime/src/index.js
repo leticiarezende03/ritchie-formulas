341c7c18dde7777a9978f709fdb8e834155c17f3
leticiasilvazup <leticia.silva@zup.com.br>
2020-09-01 20:50:50 -0300

    first commit

--- clubhouse/get/data/leadtime/src/index.js
+++
@@ -1,5 +0,0 @@
-const run = require("./formula/formula")
-
-const CLUBHOUSE_API_TOKEN = process.env.CLUBHOUSE_API_TOKEN
-
-run(CLUBHOUSE_API_TOKEN)
--- github/get/credentials/src/index.js
+++
@@ -1,6 +0,0 @@
-const run = require("./formula/formula")
-
-const INPUT1 = process.env.GIT_USER
-const INPUT2 = process.env.GIT_TOKEN
-
-run(INPUT1, INPUT2)
